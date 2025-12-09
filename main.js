9294617b7531e1a73428cabbea99358797a5f142
Arsen <arsenabdizhamiyev00@gmail.com>
2025-12-09 19:19:44 +0500

    ready to dep

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '/')));
 app.use(express.json());
 
