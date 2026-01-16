5f9e22a7fcf5629d73ea0608d5f14d11802208d8
Arsen <arsenabdizhamiyev00@gmail.com>
2026-01-16 17:16:47 +0500

    design: changed welcome text's color and added shadow to it feature: now blogs are available

---
+++ main.ts
@@ -0,0 +1,249 @@
+// imports
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import * as dotenv from 'dotenv';
+import mongoose, { Schema, Document } from 'mongoose';
+import jwt from 'jsonwebtoken';
+dotenv.config();
+
+// app init
+const app = express();
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// env vars
+const PORT = process.env.PORT ?? "3000";
+const API_KEY = process.env.API_KEY;
+const mongoDBURI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+// Connect to MongoDB
+if (!mongoDBURI) {
+    console.error('Missing required env: MONGODB_URI');
+    process.exit(1);
+}
+
+mongoose.connect(mongoDBURI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch((err: any) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
+
+// models
+interface IBlog extends Document {
+    title: string;
+    body: string;
+    author: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const blogSchema = new Schema<IBlog>({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    author: { type: String, required: true },
+}, {
+    timestamps: true // Автоматически создаст и будет обновлять createdAt и updatedAt
+});
+
+const BlogModel = mongoose.model<IBlog>('Blog', blogSchema);
+
+// middlewares
+const requestLogger = function (req: Request, res: Response, next: NextFunction) {
+    const timestamp = new Date().toISOString();
+    console.log(`${timestamp} - ${req.method} ${req.url}`);
+
+    next();
+};
+
+app.use(requestLogger);
+
+app.post("/api/calc", (req, res) => {
+    const { weight, height } = req.query;
+
+    const w = Number(weight);
+    const h = Number(height);
+
+    if (!w || !h) {
+        return res.json({ error: "Invalid weight or height" });
+    }
+
+    const heightInMeters = h / 100;
+    const bmi = w / (heightInMeters * heightInMeters);
+
+    let cat;
+    if (bmi < 18.5) {
+        cat = "Underweight";
+    } else if (bmi < 25) {
+        cat = "Normal weight";
+    } else if (bmi < 30) {
+        cat = "Overweight";
+    } else {
+        cat = "Obese";
+    }
+
+    res.json({
+        success: true,
+        weight: w,
+        height: h,
+        bmi: Number(bmi.toFixed(2)),
+        category: cat
+    });
+});
+
+app.get("/api/weather", async (req, res) => {
+    try {
+        const { lat, lon } = req.query;
+
+        if (!lat || !lon) {
+            return res.status(400).json({ error: "lat and lon required" });
+        }
+
+        if (!API_KEY) {
+            console.error('Missing required env: API_KEY');
+            process.exit(1);
+        }
+
+        const url = `https://api.openweathermap.org/data/2.5/weather` +
+            `?lat=${lat}&lon=${lon}` +
+            `&units=metric` +
+            `&lang=ru` +
+            `&appid=${API_KEY}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Weather API error" });
+    }
+});
+
+app.get("/api/news", async (req, res) => {
+    try {
+        const file = path.join(__dirname, 'static/sample_data', 'news-georgia.json');
+        const raw = await fs.promises.readFile(file, 'utf8');
+        const data = JSON.parse(raw);
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "News API error" });
+    }
+});
+
+app.get("/api/currency", async (req, res) => {
+    try {
+        const file = path.join(__dirname, 'static/sample_data', 'currency-sample.json');
+        const raw = await fs.promises.readFile(file, 'utf8');
+        const data = JSON.parse(raw);
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Currency API error" });
+    }
+});
+
+app.post("/api/login", async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.post("/api/blogs", async (req, res) => {
+    try {
+        const { title, body, author } = req.body;
+
+        const newBlog = new BlogModel({
+            title,
+            body,
+            author
+        });
+
+        const savedBlog = await newBlog.save();
+
+        console.log('Blog saved:', savedBlog);
+        res.redirect("/blog.html");
+    } catch (err: any) {
+        console.log(err.message);
+        res.status(400).json({ error: err.message });
+    }
+});
+
+app.get("/api/blogs", async (req, res) => {
+    try {
+        const blogs = await BlogModel.find().sort({ updatedAt: -1 });
+        res.status(200).json(blogs);
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+    try {
+        const blog = await BlogModel.findById(req.params.id);
+        if (!blog) {
+            return res.status(404).json({ error: "Blog not found" });
+        }
+        res.status(200).json(blog);
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.put("/api/blogs/:id", async (req, res) => {
+    try {
+        const { title, body, author } = req.body;
+
+        const updatedBlog = await BlogModel.findByIdAndUpdate(
+            req.params.id,
+            { title, body, author },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedBlog) {
+            return res.status(404).json({ error: "Blog not found" });
+        }
+
+        res.json(updatedBlog);
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+    try {
+        const deletedBlog = await BlogModel.findByIdAndDelete(req.params.id);
+        if (!deletedBlog) {
+            return res.status(404).json("Not found");
+        }
+        res.json("Blod deleted!");
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/blog/:id", async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, 'static', 'blog-x.html'));
+    } catch (err: any) {
+        res.status(500).json({ error: err.messge });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
